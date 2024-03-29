2d977794c0fd79cb0821f4576f5dda9eae3d5d63
noura <noura.tu19@gmail.com>
2024-03-29 22:39:02 +0300

    get total price

---
+++ main.js
@@ -0,0 +1,31 @@
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let submit = document.getElementById("submit");
+
+// get total price
+function getTotalPrice() {
+  if (price.value != "") {
+    let result = +price.value + +taxes.value + +ads.value - +discount.value;
+    total.innerHTML = result;
+    total.style.backgroundColor = "green";
+  } else {
+    total.innerHTML = "";
+    total.style.backgroundColor = "#a00d02";
+  }
+}
+
+// create product
+// save local storge
+// clear inputs
+// read
+// count
+//delete
+// update
+//sherch
+// clean
